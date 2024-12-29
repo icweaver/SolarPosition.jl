a274a322094aedb2dd542448d19da1a01c96ae4b
Stefan de Lange <langestefan@msn.com>
2024-12-29 21:16:37 +0100

    Apply Bestie template

---
+++ docs/make.jl
@@ -0,0 +1,21 @@
+using SolarPosition
+using Documenter
+
+DocMeta.setdocmeta!(SolarPosition, :DocTestSetup, :(using SolarPosition); recursive = true)
+
+const page_rename = Dict("developer.md" => "Developer docs") # Without the numbers
+const numbered_pages = [
+    file for file in readdir(joinpath(@__DIR__, "src")) if
+    file != "index.md" && splitext(file)[2] == ".md"
+]
+
+makedocs(;
+    modules = [SolarPosition],
+    authors = "Stefan de Lange <langestefan@msn.com>",
+    repo = "https://github.com/PVSMC/SolarPosition.jl/blob/{commit}{path}#{line}",
+    sitename = "SolarPosition.jl",
+    format = Documenter.HTML(; canonical = "https://PVSMC.github.io/SolarPosition.jl"),
+    pages = ["index.md"; numbered_pages],
+)
+
+deploydocs(; repo = "github.com/PVSMC/SolarPosition.jl")
---
+++ docs/src/index.md
@@ -0,0 +1,20 @@
+```@meta
+CurrentModule = SolarPosition
+```
+
+# SolarPosition
+
+Documentation for [SolarPosition](https://github.com/PVSMC/SolarPosition.jl).
+
+## Contributors
+
+```@raw html
+<!-- ALL-CONTRIBUTORS-LIST:START - Do not remove or modify this section -->
+<!-- prettier-ignore-start -->
+<!-- markdownlint-disable -->
+
+<!-- markdownlint-restore -->
+<!-- prettier-ignore-end -->
+
+<!-- ALL-CONTRIBUTORS-LIST:END -->
+```
