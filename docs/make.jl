668eb3948943020ff8d8a01d1a06789373687081
Stefan de Lange <langestefan@msn.com>
2024-12-29 22:08:01 +0100

    Apply SciML style

--- docs/make.jl
+++ docs/make.jl
@@ -4,10 +4,10 @@ using Documenter
 DocMeta.setdocmeta!(SolarPosition, :DocTestSetup, :(using SolarPosition); recursive = true)
 
 const page_rename = Dict("developer.md" => "Developer docs") # Without the numbers
-const numbered_pages = [
-    file for file in readdir(joinpath(@__DIR__, "src")) if
-    file != "index.md" && splitext(file)[2] == ".md"
-]
+const numbered_pages = [file
+                        for file in readdir(joinpath(@__DIR__, "src"))
+                        if
+                        file != "index.md" && splitext(file)[2] == ".md"]
 
 makedocs(;
     modules = [SolarPosition],
@@ -15,7 +15,7 @@ makedocs(;
     repo = "https://github.com/PVSMC/SolarPosition.jl/blob/{commit}{path}#{line}",
     sitename = "SolarPosition.jl",
     format = Documenter.HTML(; canonical = "https://PVSMC.github.io/SolarPosition.jl"),
-    pages = ["index.md"; numbered_pages],
+    pages = ["index.md"; numbered_pages]
 )
 
 deploydocs(; repo = "github.com/PVSMC/SolarPosition.jl")
