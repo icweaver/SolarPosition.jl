2b81ae77f77be8688adbda3083c396cdfb8da1a3
Stefan de Lange <langestefan@msn.com>
2025-10-19 13:53:43 +0200

    Increase allocation limit

--- test/test-interface.jl
+++ test/test-interface.jl
@@ -70,7 +70,7 @@ using Dates: Hour, @dateformat_str
             @test length(pos) == n_dts
 
             # test a second time to ensure there are minimal allocations
-            alloc_limit = alg isa SPA ? 200 : 32
+            alloc_limit = alg isa SPA ? 300 : 32
             @test @allocated(solar_position!(pos, obs, dts, alg)) ≤ alloc_limit
             @test all(pos.azimuth .!= 0.0)
             @test pos[1] == single_result
